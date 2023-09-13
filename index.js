1a8b47e3c0c6c998832a124f4ac50eb6788b28d9
dev-akhilesh <akhileshtakawale703@gmail.com>
2023-09-13 17:30:45 +0530

    backend structure ready

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
+const express = require('express');
+const cors = require('cors')
+const { OpenAIApi, Configuration } = require('openai');
+require('dotenv').config();
 
-app.use(cors());
+const app = express();
+app.use(cors())
 app.use(express.json());
+
+// Set your OpenAI API key
+const apiKey = process.env.OPEN_AI_KEY;
+
+app.get('/', (req, res) => {
+    res.status(200).send('Welcome To Open AI');
+})
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
